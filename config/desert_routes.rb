3ca909a2b78bf0039f3c4a56ef860f89977adde2
Josh N. Abbott <joshnabbott@gmail.com>
2009-09-22 13:21:53 -0700

    use authentication as prefix for routes

--- config/desert_routes.rb
+++ config/desert_routes.rb
@@ -1,3 +1,5 @@
-resource :account, :controller => 'users'
-resource :user_session
-resources :users, :except => [ :index ]
+with_options(:path_prefix => 'authentication') do |authentication|
+  authentication.resource :account, :controller => 'users'
+  authentication.resource :user_session
+  authentication.resources :users, :except => [ :index ]
+end
